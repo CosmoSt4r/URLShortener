b7c76a760c6349cbe8f1d8b167cafd01c5b57e0f
CosmoSt4r <andreychik228@yandex.ru>
2020-08-26 13:37:15 +0500

    Copy button fixed

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 from app import app
 from database import db
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
+    if request.method == 'POST':
+        # POST
+        try:
+            short_url = request.form['short_url']
+            # long_url = take url from database
+
+            return render_template('index.html', short_url=short_url, display='block')
+        except:
+            long_url = request.form['long_url']
+
+            if long_url:
+                if "http" not in long_url:
+                    long_url = "https://" + long_url
+                if "." not in long_url:
+                    long_url = long_url + ".com/"
+
+                short_url = long_url[:10]  # # # # #
+            else:
+                return render_template('index.html', display='none')
+
+            return render_template('index.html', short_url=short_url, long_url=long_url, display='block')
+    else:
         # GET
 
         return render_template('index.html', display='none')
-    else:
-        # POST
-        long_url = request.form['long_url']
-
-        if long_url:
-            if "http" not in long_url:
-                long_url = "https://" + long_url
-            if "." not in long_url:
-                long_url = long_url + ".com/"
-
-            short_url = long_url[:10]  # # # # #
-        else:
-            return render_template('index.html', display='none')
-
-        return render_template('index.html', short_url=short_url, long_url=long_url, display='block')
 
 
-@app.route("/<url_name>", methods=['GET', 'POST'])
+@app.route("/<url_name>")
 def url(url_name):
-    return f'<h1>{url_name}</h1>'
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
