7401f87152a8c6c94766e574b84639fae6ca6c05
CosmoSt4r <andreychik228@yandex.ru>
2020-08-26 12:35:51 +0500

    Long url catching added

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from app import app
 from database import db
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        # GET
+
+        return render_template('index.html', display='none')
+    else:
+        # POST
+        long_url = request.form['long_url']
+
+        if long_url:
+            if "http" not in long_url:
+                long_url = "https://" + long_url
+            if "." not in long_url:
+                long_url = long_url + ".com/"
+
+            short_url = long_url[:10]  # # # # #
+        else:
+            return render_template('index.html', display='none')
+
+        return render_template('index.html', short_url=short_url, long_url=long_url, display='block')
 
 
 @app.route("/<url_name>", methods=['GET', 'POST'])
