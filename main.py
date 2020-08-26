d7370e7d51f0a6e533c4f40cfad1e36bf7a9efda
CosmoSt4r <andreychik228@yandex.ru>
2020-08-26 12:05:44 +0500

    Short URL representation added to the main page

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/<url_name>", methods=['GET', 'POST'])
+def url(url_name):
+    return f'<h1>{url_name}</h1>'
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run()
